72751119954a1e5c39b1d7d68f56aa8fe77406c9
ismael <spacingismael@gmail.com>
2024-11-19 04:19:54 +0100

    testing fixing task 1 in db file#3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -37,7 +37,7 @@ class DB:
         """
         session = self._session
         try:
-            new_user: User = User(email=email, hashed_password=hashed_password)
+            new_user = User(email=email, hashed_password=hashed_password)
             session.add(new_user)
             session.commit()
         except Exception:
