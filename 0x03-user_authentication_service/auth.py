02df95730c1eeb88f280780932fd02eaa48fe9ff
ismael <spacingismael@gmail.com>
2024-11-20 16:15:25 +0100

    task 8#2

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -40,7 +40,6 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode(), user.hashed_password)
-        except Exception:
-            pass
-        return False
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode(), user.hashed_password)
