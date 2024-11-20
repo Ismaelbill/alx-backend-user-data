13dc7cedeed8800543f2c24d2ddff62df8f28a8b
ismael <spacingismael@gmail.com>
2024-11-20 20:36:31 +0100

    fixing task 14#2

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -52,7 +52,7 @@ def logout():
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
     if user:
-        AUTH.destroy_session(session_id)
+        AUTH.destroy_session(user.id)
         return redirect(url_for('home'))
     abort(403)
 
